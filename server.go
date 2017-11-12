de53fb710427886bb5321ea7685ace6e27671f7c
ginanjar azie <xstepnort@gmail.com>
2017-11-12 13:19:24 +0700

    rollback to initial condition

--- server.go
+++ server.go
@@ -1,15 +1,28 @@
 package main
-
 import (
+	"log"
+	"fmt"
 	"net/http"
-
-	"github.com/labstack/echo"
+	"os"
 )
-
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
+}
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
+}
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	addr, err := determineListenAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc("/", hello)
+	log.Printf("Listening on %s...\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
 }
